32c8cab8d777614254a573b76e6815c6ee22ed34
mkcnly <mkcnly@gmail.com>
2018-12-21 08:42:27 -0800

    added cat export

--- routes/cat.js
+++ routes/cat.js
@@ -1,2 +1,12 @@
-
-test
+const cats=[
+{
+    imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
+    imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
+    name: 'Fluffy',
+    sex: 'Female',
+    age: 2,
+    breed: 'Bengal',
+    story: 'Thrown on the street'
+  }
+]
+ module.exports= { cats }
